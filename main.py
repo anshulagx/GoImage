20e5f420fcd3363a1a0144e078e26a86868009ee
Anshul Agarwala <anshulagarwala@Anshuls-MacBook-Air.local>
2022-01-08 13:12:58 +0530

    feat update

--- main.py
+++ main.py
@@ -5,43 +5,69 @@ from flask import send_file
 from PIL import Image
 import requests
 from io import BytesIO
+from PIL import ImageFilter
 
 
 app = Flask(__name__)
 
 
-def resize(im, w, h):
-    im1 = im.resize((w, h))
-    return im1
+# def modify(im, w=None, h=None, rot=None, b=False):
+#     m_img = im
 
+#     m_img = im.resize((w, h))
+#     if(b):
+#         m_img = filter(ImageFilter.MaxFilter(3))
+#     # m_img = im.rotate(im, rot)
+#     # m_img = im.thumbnail(im, rot)
 
-def modify(im, w, h):
-    m_img = resize(im, w, h)
-    # m_img=somethibg(a,b,c)
-    return m_img
+#     return m_img
 
 
 @app.route("/")
 def home():
+    if(not request.args.get('img')):
+        return "No img"
+
     img = request.args.get('img')
-
-    # format = request.args.get('format')
-    w = int(request.args.get('w'))
-    h = int(request.args.get('h'))
-
     response = requests.get(img)
     imgg = Image.open(BytesIO(response.content))
 
-    modified_img = modify(imgg, w, h)
+    modified_img = imgg
 
-    return serve_pil_image(modified_img)
+    # size formating
+    if(request.args.get('w') and request.args.get('h')):
+        w = int(request.args.get('w'))
+        h = int(request.args.get('h'))
+        modified_img = modified_img.resize((w, h))
 
+    if(request.args.get('rot')):
+        rot = int(request.args.get('rot'))
+        modified_img = modified_img.rotate(rot)
 
-def serve_pil_image(pil_img):
+    # if(request.args.get('b') != None):
+    #     b = request.args.get('b')
+    #     modified_img = modified_img.filter(ImageFilter.MaxFilter(3))
+
+    q = 100
+    if(request.args.get('q')):
+        q = int(request.args.get('q'))
+
+    f = "JPEG"
+    if(request.args.get('f')):
+        f = request.args.get('f')
+
     img_io = BytesIO()
-    pil_img.save(img_io, 'JPEG', quality=70)
+    modified_img.save(img_io, f, quality=q)
     img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
+    return send_file(img_io, mimetype='image/'+f.lower())
+
+
+def serve_pil_image(pil_img, q=100, f="JPEG"):
+    print("g", f)
+    img_io = BytesIO()
+    pil_img.save(img_io, f, quality=q)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/'+f.lower())
 
 
 if __name__ == "__main__":
